8a1788426f69e4004419a7bd80d8b3b72576b5c5
Seth Coussens <seth.coussens@gmail.com>
2018-05-17 00:00:11 -0500

    update port

--- src/server.ts
+++ src/server.ts
@@ -32,6 +32,8 @@ const app = express();
 
 app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
 app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
-app.listen(3000, () => {
-    console.log('Go to http://localhost:3000/graphiql to run quieries!');
+
+let port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log("Go to http://localhost:%d/graphiql to run quieries!", port);
 });
