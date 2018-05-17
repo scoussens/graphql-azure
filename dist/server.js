bac83f391eba91be428ed2060316cf9e7212c983
Seth Coussens <seth.coussens@gmail.com>
2018-05-17 11:40:23 -0500

    switched to graphql yoga

--- dist/server.js
+++ dist/server.js
@@ -13,4 +13,4 @@ const resolvers = {
 };
 const server = new graphql_yoga_1.GraphQLServer({ typeDefs, resolvers });
 let port = process.env.port || 4300;
-server.start({ port }, () => console.log('Server is running on localhost:4000'));
+server.start({ port }, () => console.log("Server is running on localhost:%d", port));
--- src/server.ts
+++ src/server.ts
@@ -4,14 +4,14 @@ const typeDefs = `
   type Query {
     hello(name: String): String!
   }
-`
+`;
 
 const resolvers = {
   Query: {
     hello: (_: any, { name }: any) => `Hello ${name || 'World'}`,
   },
-}
+};
 
 const server = new GraphQLServer({ typeDefs, resolvers });
 let port = process.env.port || 4300;
-server.start({port}, () => console.log('Server is running on localhost:4000'))
+server.start({port}, () => console.log("Server is running on localhost:%d", port))
